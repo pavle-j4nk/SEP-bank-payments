966dacf233460efe426d06a230da518b7f2dd5bd
Pavle Jankovic <pavle.gp@gmail.com>
2020-01-31 13:22:19 +0100

    Frontend

---
+++ src/main/resources/static/precache-manifest.b983a5858d0c56737736eaffe8626ff6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d128a0335526e53bff63209347d03182",
+    "url": "/bank/index.html"
+  },
+  {
+    "revision": "02ee4e8028019bd2dcb7",
+    "url": "/bank/static/css/main.c135d15c.chunk.css"
+  },
+  {
+    "revision": "ddde216b724f1f975629",
+    "url": "/bank/static/js/2.959ea793.chunk.js"
+  },
+  {
+    "revision": "f8513fb9f2981a1b206fb733b985f272",
+    "url": "/bank/static/js/2.959ea793.chunk.js.LICENSE"
+  },
+  {
+    "revision": "02ee4e8028019bd2dcb7",
+    "url": "/bank/static/js/main.fd1a269c.chunk.js"
+  },
+  {
+    "revision": "ab2e572b5d97c28511f0",
+    "url": "/bank/static/js/runtime-main.7d9fc804.js"
+  }
+]);
